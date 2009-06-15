2e014f200768c91a1504002af1be5c0ac5eb0f42
Kevin Smith <git@kismith.co.uk>
2009-06-15 21:26:17 +0100

    setDocumentMode for prettier Mac tabs.

--- Swift/QtUI/QtChatTabs.cpp
+++ Swift/QtUI/QtChatTabs.cpp
@@ -8,6 +8,10 @@
 namespace Swift {
 QtChatTabs::QtChatTabs() {
 	tabs_ = new QTabWidget(this);
+#if QT_VERSION >= 0x040500
+	//For Macs, change the tab rendering.
+	tabs_->setDocumentMode(true);
+#endif
 	QVBoxLayout *layout = new QVBoxLayout;
 	layout->setSpacing(0);
 	layout->setContentsMargins(0, 3, 0, 0);
