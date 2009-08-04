8de45cd97339e94ae0dc120f068248b5795e118b
Remko Tronçon <git@el-tramo.be>
2009-08-04 10:39:04 +0200

    CocoaAction: Remove redundant Menulet declaration.

--- Slimber/Cocoa/CocoaAction.h
+++ Slimber/Cocoa/CocoaAction.h
@@ -3,13 +3,18 @@
 #include <Cocoa/Cocoa.h>
 #include <boost/function.hpp>
 
-class CocoaMenulet;
-
 @interface CocoaAction : NSObject {
 	boost::function<void ()>* function;
 }
 
+/**
+ * Acquires ownership of 'f'.
+ */
 - (id) initWithFunction: (boost::function<void()>*) f;
+
+/**
+ * Calls the functor passed as a parameter to the contsructor.
+ */
 - (void) doAction: (id) sender;
 
 @end
