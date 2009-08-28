bb80efbacd3e02728c3a9cbbf3c73ac4beeb997e
Remko Tronçon <git@el-tramo.be>
2009-08-28 22:18:22 +0200

    Make version tool work on Windows.

--- BuildTools/SCons/Version.py
+++ BuildTools/SCons/Version.py
@@ -1,7 +1,7 @@
-import subprocess
+import subprocess, os
 
 def getGitBuildVersion() :
-  p = subprocess.Popen("git rev-parse HEAD", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
+  p = subprocess.Popen("git rev-parse HEAD", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=(os.name != "nt"))
   return p.stdout.read().rstrip()[0:7]
 
 def getBuildVersion(version = None) :
