7ff184d4c980752d6e0c4f38a2eaadce4dead6f2
Remko Tronçon <git@el-tramo.be>
2009-11-01 16:08:17 +0100

    Fix incorrectly bundled Qt image plugins.

--- BuildTools/SCons/Tools/WindowsBundle.py
+++ BuildTools/SCons/Tools/WindowsBundle.py
@@ -1,10 +1,12 @@
 import SCons.Util, os
 
 def generate(env) :
-  def createWindowsBundle(env, bundle, resources = [], qtlibs = []) :
+  def createWindowsBundle(env, bundle, resources = [], qtimageformats = [], qtlibs = []) :
     env.Install(bundle, bundle + ".exe")
     for lib in qtlibs :
       env.Install(bundle, os.path.join(env["QTDIR"], "bin", lib + ".dll"))
+    env.Install(os.path.join(bundle, "imageformats"), [os.path.join(env["QTDIR"], "plugins", "imageformats", "q" + codec + "4.dll") for codec in qtimageformats])
+
     for resource in resources :
       env.Install(bundle, resource)
 
