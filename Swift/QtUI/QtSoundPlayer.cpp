8149107ade43f9c9fff8fe134f1bce5b5e8b2234
Remko Tronçon <git@el-tramo.be>
2009-11-20 19:27:23 +0100

    Use QSound for sounds.

--- Swift/QtUI/QtSoundPlayer.cpp
+++ Swift/QtUI/QtSoundPlayer.cpp
@@ -1,40 +1,18 @@
 #include "QtSoundPlayer.h"
 
-#ifdef Q_WS_X11
-#include "mediaobject.h"
-#include "audiooutput.h"
-#else
-#include <phonon/MediaObject>
-#include <phonon/AudioOutput>
-#endif
+#include <QSound>
 
-namespace Swift{
+namespace Swift {
 	
 QtSoundPlayer::QtSoundPlayer() {
-	audioOutput_ = new Phonon::AudioOutput(Phonon::NotificationCategory);
-	
-	messageReceived_ = new Phonon::MediaObject();
-	messageReceived_->setCurrentSource(Phonon::MediaSource(":/sounds/message-received.wav"));
-	Phonon::createPath(messageReceived_, audioOutput_);
-	connect(messageReceived_, SIGNAL(finished()), this, SLOT(handleFinished()));
 }
 
-QtSoundPlayer::~QtSoundPlayer() {
-	delete messageReceived_;
-	delete audioOutput_;
-}
-	
 void QtSoundPlayer::playSound(SoundEffect sound) {
 	switch (sound) {
 		case MessageReceived: 
-			//messageReceived_->stop();
-			messageReceived_->play();
+			QSound::play(":/sounds/message-received.wav");
 			break;
 	}
-}
-
-void QtSoundPlayer::handleFinished() {
-	messageReceived_->stop();
 }
 
 }
--- Swift/QtUI/QtSoundPlayer.h
+++ Swift/QtUI/QtSoundPlayer.h
@@ -4,22 +4,12 @@
 
 #include <QObject>
 
-namespace Phonon {
-	class AudioOutput;
-	class MediaObject;
-}
-
 namespace Swift {
 	class QtSoundPlayer : public QObject, public SoundPlayer {
-		Q_OBJECT
+			Q_OBJECT
 		public:
 			QtSoundPlayer();
-			~QtSoundPlayer();
+
 			void playSound(SoundEffect sound);
-		private:
-			Phonon::AudioOutput* audioOutput_;
-			Phonon::MediaObject* messageReceived_;
-		private slots:
-			void handleFinished();
 	};
 }
