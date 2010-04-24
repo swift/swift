b40812585475ca7795a115774345ed80947134fa
Kevin Smith <git@kismith.co.uk>
2010-04-24 09:41:34 +0000

    Don't segfault on unknown command-line parameters.
    Resolves: #327

--- Swift/QtUI/main.cpp
+++ Swift/QtUI/main.cpp
@@ -8,17 +8,26 @@
 #include <boost/program_options/variables_map.hpp>
 #include <boost/program_options.hpp>
 
+#include <iostream>
 
 #include <QApplication>
 
 #include "QtSwift.h"
 
 int main(int argc, char* argv[]) {
-	QApplication app(argc, argv);
 	boost::program_options::options_description desc = Swift::QtSwift::getOptionsDescription();
-	boost::program_options::variables_map vm;	
-	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+	boost::program_options::variables_map vm;
+	try {
+		boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+	} catch (boost::program_options::unknown_option option) {
+		std::cout << "Ignoring unknown option " << option.get_option_name() << " but continuing." <<  std::endl;
+	}
 	boost::program_options::notify(vm);
+	if (vm.count("help") > 0) {
+		std::cout << desc << "\n";
+		return 1;
+	}
+	QApplication app(argc, argv);
 	Swift::QtSwift swift(vm);
 	return app.exec();
 }
