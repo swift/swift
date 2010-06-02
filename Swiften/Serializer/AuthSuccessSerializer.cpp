3362805ffb92452a82b6ca5866377cd85fae764f
Remko Tronçon <git@el-tramo.be>
2010-06-02 21:05:43 +0200

    Serialize <success/> stanza correctly.

--- Swiften/Serializer/AuthSuccessSerializer.cpp
+++ Swiften/Serializer/AuthSuccessSerializer.cpp
@@ -17,7 +17,7 @@ AuthSuccessSerializer::AuthSuccessSerializer() {
 String AuthSuccessSerializer::serialize(boost::shared_ptr<Element> element)  const {
 	boost::shared_ptr<AuthSuccess> authRequest(boost::dynamic_pointer_cast<AuthSuccess>(element));
 	String value = (authRequest->getValue().isEmpty() ? "=" : Base64::encode(authRequest->getValue()));
-	return "<success xmlns=\"urn:ietf:params:xml:ns:xmpp-sasl\">" + value + "</challenge>";
+	return "<success xmlns=\"urn:ietf:params:xml:ns:xmpp-sasl\">" + value + "</success>";
 }
 
 }
