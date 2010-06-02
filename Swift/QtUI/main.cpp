23eff5bbb49f1402139065948db7b5b6535d1abd
Remko Tronçon <git@el-tramo.be>
2010-06-02 22:16:59 +0200

    Don't use unknown_program_option.get_option_name() on older Boosts.

--- Swift/QtUI/main.cpp
+++ Swift/QtUI/main.cpp
@@ -7,6 +7,7 @@
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/variables_map.hpp>
 #include <boost/program_options.hpp>
+#include <boost/version.hpp>
 
 #include <iostream>
 
@@ -20,7 +21,11 @@ int main(int argc, char* argv[]) {
 	try {
 		boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
 	} catch (boost::program_options::unknown_option option) {
+#if BOOST_VERSION >= 104200
 		std::cout << "Ignoring unknown option " << option.get_option_name() << " but continuing." <<  std::endl;
+#else
+		std::cout << "Error: " << option.what() << " (continuing)" <<  std::endl;
+#endif
 	}
 	boost::program_options::notify(vm);
 	if (vm.count("help") > 0) {
