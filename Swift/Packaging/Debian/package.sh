9f542c719460650f1eb63b651ff1576362e5eeac
Remko Tronçon <git@el-tramo.be>
2010-06-14 21:08:57 +0200

    Remove SCons from Debian sources.

--- Swift/Packaging/Debian/package.sh
+++ Swift/Packaging/Debian/package.sh
@@ -12,11 +12,13 @@ fi
 
 echo "Cleaning up old packages ..."
 rm swift_*
+rm -rf swift-*
 
 echo "Checking out a fresh copy ..."
 rm -rf $DIRNAME
 git clone ../../../.git $DIRNAME
 find $DIRNAME/3rdParty/Boost/src -type f | grep -v uuid | xargs rm -f
+find $DIRNAME/3rdParty/SCons -delete
 ln -s Swift/Packaging/Debian/debian $DIRNAME/debian
 ../../../BuildTools/UpdateDebianChangelog.py $DIRNAME/debian/changelog $DEBIAN_VERSION
 
