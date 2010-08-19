72d22d91c99e2b1bd3c2abc712195111a434fcfc
Remko Tronçon <git@el-tramo.be>
2010-08-19 20:46:16 +0200

    Added missing file.

---
+++ Swiften/Application/WindowsApplicationPathProvider.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2010 Remko Tronçon
+ * Licensed under the GNU General Public License v3.
+ * See Documentation/Licenses/GPLv3.txt for more information.
+ */
+
+#include "Swiften/Application/WindowsApplicationPathProvider.h"
+
+namespace Swift {
+
+WindowsApplicationPathProvider::WindowsApplicationPathProvider(const String& name) : ApplicationPathProvider(name) {
+}
+
+}
