2f3d74e471022d21477adccf013a0430956bddbf
Remko Tronçon <git@el-tramo.be>
2010-08-30 19:13:35 +0200

    Track dependencies of resource dirs correctly in Windows bundle.

--- BuildTools/SCons/Tools/WindowsBundle.py
+++ BuildTools/SCons/Tools/WindowsBundle.py
@@ -8,7 +8,12 @@ def generate(env) :
     env.Install(os.path.join(bundle, "imageformats"), [os.path.join(env["QTDIR"], "plugins", "imageformats", "q" + codec + "4.dll") for codec in qtimageformats])
 
     for resource in resources :
-      env.Install(bundle, resource)
+      e = env.Entry(resource)
+      if e.isdir() :
+	for subresource in env.Glob(str(e) + "/*") :
+      	  env.Install(os.path.join(bundle, e.name), subresource)
+      else :
+      	env.Install(bundle, resource)
 
   env.AddMethod(createWindowsBundle, "WindowsBundle")
 
