bc3b2a2e7a70e597390a464ed19a564512f96e69
Remko Tronçon <git@el-tramo.be>
2010-09-03 20:40:10 +0200

    Added SCons flag to ignore test result.

--- BuildTools/SCons/Tools/Test.py
+++ BuildTools/SCons/Tools/Test.py
@@ -9,9 +9,12 @@ def generate(env) :
       # Special support for unittest checker
       if is_checker and env.get("checker_report", False) :
         params = " --xml > " + os.path.join(target[0].dir.path, "checker-report.xml")
-
+	
+      ignore_prefix = ""
+      if env.get("TEST_IGNORE_RESULT", False) :
+        ignore_prefix = "-"
       env.Command("**dummy**", target, 
-          SCons.Action.Action(env.get("TEST_RUNNER", "") + cmd + " " + params, cmdstr = "$TESTCOMSTR"))
+          SCons.Action.Action(ignore_prefix + env.get("TEST_RUNNER", "") + cmd + " " + params, cmdstr = "$TESTCOMSTR"))
 
   env.AddMethod(registerTest, "Test")
 
