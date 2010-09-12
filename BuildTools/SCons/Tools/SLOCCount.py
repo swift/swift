04f1fcc7cec93068afc7270297f9fea9ca5f49f5
Remko Tronçon <git@el-tramo.be>
2010-09-12 12:26:43 +0200

    Ignore .qrc in sloc counts.

--- BuildTools/SCons/Tools/SLOCCount.py
+++ BuildTools/SCons/Tools/SLOCCount.py
@@ -9,7 +9,7 @@ def generate(env) :
     # FIXME: There's probably a better way to force building the .sc
     if os.path.exists(target.abspath) :
       os.unlink(target.abspath)
-    return myenv.Command(target, source, [SCons.Action.Action("sloccount --duplicates --wide --details " + source.path + " > $TARGET", cmdstr = "$GENCOMSTR")])
+    return myenv.Command(target, source, [SCons.Action.Action("sloccount --duplicates --wide --details " + source.path + " | grep -v qrc_ > $TARGET", cmdstr = "$GENCOMSTR")])
 
   env.AddMethod(createSLOCCount, "SLOCCount")
 
