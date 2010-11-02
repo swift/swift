badcdcb7456d38016ea1746d6754a40dd9646813
Remko Tronçon <git@el-tramo.be>
2010-11-02 20:45:10 +0100

    Copy uuid/ dir in packaging script.

--- Swift/Packaging/Debian/package.sh
+++ Swift/Packaging/Debian/package.sh
@@ -22,6 +22,10 @@ git clone ../../../.git $DIRNAME
 find $DIRNAME/3rdParty/Boost/src -type f | grep -v uuid | xargs rm -f
 find $DIRNAME/3rdParty/SCons -delete
 find $DIRNAME/3rdParty/DocBook -delete
+# FIXME: This shouldn't be necessary, but SCons isn't picking up the generated headers for compilation
+mkdir -p $DIRNAME/3rdParty/Boost/uuid/boost
+cp -r $DIRNAME/3rdParty/Boost/src/boost/uuid $DIRNAME/3rdParty/Boost/uuid/boost
+
 ln -s Swift/Packaging/Debian/debian $DIRNAME/debian
 ../../../BuildTools/UpdateDebianChangelog.py $DIRNAME/debian/changelog $DEBIAN_VERSION
 
