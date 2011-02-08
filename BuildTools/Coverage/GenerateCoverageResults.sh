3e13fa1f097ec3d350e131e0826d8f97f700917b
Remko Tronçon <git@el-tramo.be>
2011-02-08 22:36:05 +0100

    Added extra parameter support to coverage script.

--- BuildTools/Coverage/GenerateCoverageResults.sh
+++ BuildTools/Coverage/GenerateCoverageResults.sh
@@ -6,8 +6,11 @@ SOURCE_DIR=.
 SCRIPT_DIR=BuildTools/Coverage
 LCOVDIR=3rdParty/LCov
 
-RESULTS_DIR=BuildTools/Coverage/results
-OUTPUT_DIR=$RESULTS_DIR/coverage-`git log --pretty=format:%ct-%h | head -n 1`
+#RESULTS_DIR=BuildTools/Coverage/results
+#OUTPUT_DIR=$RESULTS_DIR/coverage-`git log --pretty=format:%ct-%h | head -n 1`
+
+OUTPUT_DIR=BuildTools/Coverage/coverage
+rm -rf $OUTPUT_DIR
 
 if [ ! -d $OUTPUT_DIR ]; then
 	mkdir -p $OUTPUT_DIR
@@ -17,7 +20,7 @@ fi
 $LCOVDIR/lcov --zerocounters --directory $SOURCE_DIR
 
 # Build & run all tests
-scons coverage=1 test=all
+./scons coverage=1 test=all $@
 
 # Run SCons
 $LCOVDIR/lcov --capture --directory $SOURCE_DIR -b $SOURCE_DIR --output-file $OUTPUT_DIR/all.info --test-name all
