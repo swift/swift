d318d12a8fba5cf86db5750d0ab46db160bd5fe4
Remko Tronçon <git@el-tramo.be>
2011-02-24 22:56:52 +0100

    Removed obsolete example script.

--- Sluift/example.lua
+++
@@ -1,6 +0,0 @@
-require "sluift"
-
-c = sluift.connect(os.getenv("SWIFT_CLIENTTEST_JID"), os.getenv("SWIFT_CLIENTTEST_PASS"))
-v = c:get_version("swift.im")
-print(v["name"], v["version"], v["os"])
-c:disconnect()
