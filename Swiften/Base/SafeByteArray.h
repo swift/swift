9a511892f6be7a8c998c1862d50aad274535636a
Remko Tronçon <git@el-tramo.be>
2011-05-18 19:10:47 +0200

    Copyright fix.

--- Swiften/Base/SafeByteArray.h
+++ Swiften/Base/SafeByteArray.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 Remko Tronçon
+ * Copyright (c) 2011 Remko Tronçon
  * Licensed under the GNU General Public License v3.
  * See Documentation/Licenses/GPLv3.txt for more information.
  */
