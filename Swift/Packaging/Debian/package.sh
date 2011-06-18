5c2de222219de09ea0baf4f078915ee2f747bf4f
Remko Tronçon <git@el-tramo.be>
2011-06-18 22:44:26 +0200

    Tweak help documentation and add manpages.

--- Swift/Packaging/Debian/package.sh
+++ Swift/Packaging/Debian/package.sh
@@ -40,6 +40,7 @@ ln -s Swift/Packaging/Debian/debian $DIRNAME/debian
 ../../../BuildTools/UpdateDebianChangelog.py $DIRNAME/debian/changelog $DEBIAN_VERSION
 cat $DIRNAME/debian/control.in | sed -e "s/%SWIFTEN_SOVERSION%/$SWIFTEN_SOVERSION/g" > $DIRNAME/debian/control
 mv $DIRNAME/debian/libswiften.install $DIRNAME/debian/libswiften$SWIFTEN_SOVERSION.install
+mv $DIRNAME/debian/libswiften.manpages $DIRNAME/debian/libswiften$SWIFTEN_SOVERSION.manpages
 mv $DIRNAME/debian/libswiften-dev.install $DIRNAME/debian/libswiften$SWIFTEN_SOVERSION-dev.install
 
 # Build
