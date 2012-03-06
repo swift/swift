6e219bf4a63459461a0a6009b1e3dafedda9e033
Kevin Smith <git@kismith.co.uk>
2012-03-06 09:19:06 +0000

    Fix missing include
    Resolves: #903

--- Swiften/TLS/CertificateVerificationError.h
+++ Swiften/TLS/CertificateVerificationError.h
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2010 Remko Tronçon
+ * Copyright (c) 2010-2012 Remko Tronçon
  * Licensed under the GNU General Public License v3.
  * See Documentation/Licenses/GPLv3.txt for more information.
  */
 
 #pragma once
 
+#include <boost/shared_ptr.hpp>
 #include <Swiften/Base/Error.h>
 
 namespace Swift {
