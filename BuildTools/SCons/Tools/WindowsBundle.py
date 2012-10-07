b01e339e89143684f7f1406bf3ce07481e98b5bc
Remko Tronçon <git@el-tramo.be>
2012-10-07 11:10:30 +0200

    Fixed & cleaned up some of the WiX stuff.

--- BuildTools/SCons/Tools/WindowsBundle.py
+++ BuildTools/SCons/Tools/WindowsBundle.py
@@ -2,19 +2,21 @@ import SCons.Util, os
 
 def generate(env) :
   def createWindowsBundle(env, bundle, resources = {}, qtimageformats = [], qtlibs = []) :
-    env.Install(bundle, bundle + ".exe")
+    all_files = []
+    all_files += env.Install(bundle, bundle + ".exe")
     for lib in qtlibs :
-      env.Install(bundle, os.path.join(env["QTDIR"], "bin", lib + ".dll"))
-    env.Install(os.path.join(bundle, "imageformats"), [os.path.join(env["QTDIR"], "plugins", "imageformats", "q" + codec + "4.dll") for codec in qtimageformats])
+      all_files += env.Install(bundle, os.path.join(env["QTDIR"], "bin", lib + ".dll"))
+    all_files += env.Install(os.path.join(bundle, "imageformats"), [os.path.join(env["QTDIR"], "plugins", "imageformats", "q" + codec + "4.dll") for codec in qtimageformats])
 
     for dir, resourceFiles in resources.items() :
       for resource in resourceFiles :
 	e = env.Entry(resource)
 	if e.isdir() :
 	  for subresource in env.Glob(str(e) + "/*") :
-	    env.Install(os.path.join(bundle, dir, e.name), subresource)
+	    all_files += env.Install(os.path.join(bundle, dir, e.name), subresource)
 	else :
-	  env.Install(os.path.join(bundle, dir), resource)
+	  all_files += env.Install(os.path.join(bundle, dir), resource)
+    return all_files
 
   env.AddMethod(createWindowsBundle, "WindowsBundle")
 
