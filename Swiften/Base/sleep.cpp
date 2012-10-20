698d56b68533760931ea6e1509fea73cdcf35b67
Remko Tronçon <git@el-tramo.be>
2012-10-20 15:22:37 +0200

    Fixed compilation problems against Boost >= 1.50.0.
    Change-Id: I7c7c3456c9007caffda1a6b4b721c01aa47d0a6d

--- Swiften/Base/sleep.cpp
+++ Swiften/Base/sleep.cpp
@@ -7,12 +7,17 @@
 #include <Swiften/Base/sleep.h>
 
 #include <boost/thread.hpp>
+#include <boost/version.hpp>
 
 namespace Swift {
 
 void sleep(unsigned int msecs) {
 	boost::xtime xt;
+#if BOOST_VERSION >= 105000
+	boost::xtime_get(&xt, boost::TIME_UTC_);
+#else
 	boost::xtime_get(&xt, boost::TIME_UTC);
+#endif
 	xt.nsec += msecs*1000000;
 	boost::thread::sleep(xt);
 }
