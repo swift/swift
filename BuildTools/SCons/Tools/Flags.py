d6bb5d79e3726d2aad82b52fe637030177b4950f
Remko Tronçon <git@el-tramo.be>
2012-12-09 17:21:40 +0100

    Replace MergeFlags calls by UseFlags calls.
    Change-Id: If7193006987fd86d102e5e17aac426ef1a6ccd83

--- BuildTools/SCons/Tools/Flags.py
+++ BuildTools/SCons/Tools/Flags.py
@@ -3,7 +3,10 @@ import SCons.Util
 def generate(env) :
   def useFlags(env, flags) :
     for flag in flags :
-      env[flag] = env.get(flag, []) + flags[flag]
+      if flag in env :
+        env[flag] = env[flag] + flags[flag]
+      else :
+        env[flag] = flags[flag]
   env.AddMethod(useFlags, "UseFlags")
 
 def exists(env) :
