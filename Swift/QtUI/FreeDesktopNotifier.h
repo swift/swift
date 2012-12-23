feab54913e13c9a1af999039d473fce75d39be1b
Remko Tronçon <git@el-tramo.be>
2012-12-23 18:09:52 +0100

    Remove warning in FreeDesktopNotifier.
    We don't use callbacks, so purgeCallbacks is correct.
    
    Change-Id: I711f37380888791d5ccc178e13039163b4c193db

--- Swift/QtUI/FreeDesktopNotifier.h
+++ Swift/QtUI/FreeDesktopNotifier.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 Remko Tronçon
+ * Copyright (c) 2010 -2012 Remko Tronçon
  * Licensed under the GNU General Public License v3.
  * See Documentation/Licenses/GPLv3.txt for more information.
  */
@@ -15,9 +15,8 @@ namespace Swift {
 			FreeDesktopNotifier(const std::string& name);
 
 			virtual void showMessage(Type type, const std::string& subject, const std::string& description, const boost::filesystem::path& picture, boost::function<void()> callback);
-			virtual void purgeCallbacks() {
-#warning FIXME implement.
-			};
+			virtual void purgeCallbacks() {}
+
 		private:
 			std::string applicationName;
 			QtCachedImageScaler imageScaler;
