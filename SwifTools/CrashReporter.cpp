40c9a08061f285cbc4754bc135539d46c70e02e9
Remko Tronçon <git@el-tramo.be>
2012-12-23 18:55:40 +0100

    Fix compilation of CrashReporter.
    Change-Id: I7440eedcf6304ff142029324450af35a5b9606f0

--- SwifTools/CrashReporter.cpp
+++ SwifTools/CrashReporter.cpp
@@ -52,12 +52,14 @@ CrashReporter::CrashReporter(const boost::filesystem::path& path) {
 #if defined(SWIFTEN_PLATFORM_WINDOWS)
 	// FIXME: Need UTF8 conversion from string to wstring
 	std::string pathString = path.string();
-	p->handler = boost::make_shared<google_breakpad::ExceptionHandler>(
-			std::wstring(pathString.begin(), pathString.end()), 
-			(google_breakpad::ExceptionHandler::FilterCallback) 0, 
-			handleDump, 
-			(void*) 0, 
-			google_breakpad::ExceptionHandler::HANDLER_ALL);
+	p->handler = boost::shared_ptr<google_breakpad::ExceptionHandler>(
+			// Not using make_shared, because 'handleDump' seems to have problems with VC2010
+			new google_breakpad::ExceptionHandler(
+				std::wstring(pathString.begin(), pathString.end()), 
+				(google_breakpad::ExceptionHandler::FilterCallback) 0, 
+				handleDump, 
+				(void*) 0, 
+				google_breakpad::ExceptionHandler::HANDLER_ALL));
 // Turning it off for Mac, because it doesn't really help us
 //#elif defined(SWIFTEN_PLATFORM_MACOSX)
 //	p->handler = boost::make_shared<google_breakpad::ExceptionHandler>(path.string(), (google_breakpad::ExceptionHandler::FilterCallback) 0, handleDump, (void*) 0, true, (const char*) 0);
