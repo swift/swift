cd34f859f21ed5a27653f61cb57ff8aeb4acec77
Remko Tronçon <git@el-tramo.be>
2013-02-10 13:24:49 +0100

    Turn off override support for GCC in non-c++11 mode.
    Change-Id: Id4e7394c12f1bf980833dd3e9668aa00950e709f

--- Swiften/Base/Override.h
+++ Swiften/Base/Override.h
@@ -14,7 +14,7 @@
 # endif
 
 #elif defined(__GNUC__)
-# if (__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 7))
+# if ((__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 7))) && defined(__GXX_EXPERIMENTAL_CXX0X__)
 #  define SWIFTEN_OVERRIDE override
 # else
 #  define SWIFTEN_OVERRIDE
