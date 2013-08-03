85e23f6d04ea4d31a325f5267c78cbba2cbbac92
Kevin Smith <git@kismith.co.uk>
2013-08-03 10:27:53 +0100

    Missing include
    Change-Id: Ib49e3324e594d95b8c58a3a02a43d1334bf54392

--- Swift/Controllers/UIInterfaces/ChatWindow.h
+++ Swift/Controllers/UIInterfaces/ChatWindow.h
@@ -11,6 +11,7 @@
 
 #include <boost/optional.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
 #include <Swiften/Base/boost_bsignals.h>
