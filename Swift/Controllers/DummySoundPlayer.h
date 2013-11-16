ec57e1f727d3b8d64ca5bf60f2bd277cd0f23f21
Kevin Smith <git@kismith.co.uk>
2013-11-16 22:37:05 +0000

    Update dummy implementations to current API
    Change-Id: Iff64972fc9127851d4cc81e1177d78132f27ad3b

--- Swift/Controllers/DummySoundPlayer.h
+++ Swift/Controllers/DummySoundPlayer.h
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2010 Remko Tronçon
+ * Copyright (c) 2010-2013 Remko Tronçon
  * Licensed under the GNU General Public License v3.
  * See Documentation/Licenses/GPLv3.txt for more information.
  */
 
 #pragma once
 
-#include "Swift/Controllers/SoundPlayer.h"
+#include <Swift/Controllers/SoundPlayer.h>
 
 namespace Swift {
 	class DummySoundPlayer : public SoundPlayer {
 		public:
-			void playSound(SoundEffect sound) {}
+			void playSound(SoundEffect sound, const std::string& soundResource) {}
 	};
 }
--- Swift/Controllers/Storages/MemoryStoragesFactory.h
+++ Swift/Controllers/Storages/MemoryStoragesFactory.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 Remko Tronçon
+ * Copyright (c) 2010-2013 Remko Tronçon
  * Licensed under the GNU General Public License v3.
  * See Documentation/Licenses/GPLv3.txt for more information.
  */
@@ -11,13 +11,16 @@
 
 namespace Swift {
 	class JID;
+    class CryptoProvider;
 	
 	class MemoryStoragesFactory : public StoragesFactory {
 		public:
-			MemoryStoragesFactory() {}
+            MemoryStoragesFactory(CryptoProvider* cryptoProvider) : cryptoProvider_(cryptoProvider) {}
 
 			virtual Storages* createStorages(const JID& profile) const {
-				return new MemoryStorages();
+				return new MemoryStorages(cryptoProvider_);
 			}
+        private:
+            CryptoProvider* cryptoProvider_;
 	};
 }
