fac7752a353240c9e48baa53ef044323dee447ee
Remko Tronçon <git@el-tramo.be>
2014-01-26 19:45:00 +0100

    Update copyright of several files
    Change-Id: I09739d53d67d034e15d3ca40406df1255b51afef

--- Sluift/ElementConvertors/PubSubUnsubscribeConvertor.cpp
+++ Sluift/ElementConvertors/PubSubUnsubscribeConvertor.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2013 Remko Tronçon
- * Licensed under the GNU General Public License.
- * See the COPYING file for more information.
+ * Licensed under the GNU General Public License v3.
+ * See Documentation/Licenses/GPLv3.txt for more information.
  */
 
 #include <Sluift/ElementConvertors/PubSubUnsubscribeConvertor.h>
--- Swiften/Elements/SecurityLabel.cpp
+++ Swiften/Elements/SecurityLabel.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2010-2014 Remko Tronçon
- * Licensed under the GNU General Public License.
- * See the COPYING file for more information.
+ * Licensed under the GNU General Public License v3.
+ * See Documentation/Licenses/GPLv3.txt for more information.
  */
 
 #include <Swiften/Elements/SecurityLabel.h>
