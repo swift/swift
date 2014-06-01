38d994aa83b068f93189e91fe466387759f7451b
Kevin Smith <git@kismith.co.uk>
2014-06-01 13:16:28 +0100

    Fix .deb packaging
    Contcatenating the .. onto the filename breaks os.path.dirname.
    
    Change-Id: Ib018276166c9554c9adf53342d9c8bc5f954d13d

--- BuildTools/GetBuildVersion.py
+++ BuildTools/GetBuildVersion.py
@@ -11,11 +11,11 @@ if "--major" in sys.argv :
   only_major = True
 
 if only_major :
-  v = Version.getBuildVersion(os.path.dirname(sys.argv[0] + "/.."), sys.argv[1])
+  v = Version.getBuildVersion(os.path.dirname(sys.argv[0]) + "/..", sys.argv[1])
   version_match = re.match("(\d+)\.(\d+).*", v)
   if version_match :
     print version_match.group(1)
   else :
     print "0"
 else :
-  print Version.getBuildVersion(os.path.dirname(sys.argv[0] + "/.."), sys.argv[1])
+  print Version.getBuildVersion(os.path.dirname(sys.argv[0]) + "/..", sys.argv[1])
