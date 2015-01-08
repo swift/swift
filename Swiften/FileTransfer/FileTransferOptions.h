4b2c07264f6cfccf8ed577c9b68b5b237e528be0
Tobias Markmann <tm@ayena.de>
2015-01-08 22:59:56 +0100

    Extend file transfer options class with proxy, assisted and direct options.
    Change-Id: Icfa2a22aa3c443d2a400a3466bf523a8c91ba85a

--- Swiften/FileTransfer/FileTransferOptions.h
+++ Swiften/FileTransfer/FileTransferOptions.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014 Isode Limited.
+ * Copyright (c) 2013-2015 Isode Limited.
  * All rights reserved.
  * See the COPYING file for more information.
  */
@@ -12,23 +12,55 @@
 namespace Swift {
 	class SWIFTEN_API FileTransferOptions {
 		public:
-			FileTransferOptions() : allowInBand(true) {
+			FileTransferOptions() : allowInBand_(true), allowAssisted_(true), allowProxied_(true), allowDirect_(true) {
 			}
 			SWIFTEN_DEFAULT_COPY_CONSTRUCTOR(FileTransferOptions)
 			~FileTransferOptions();
 
 			FileTransferOptions& withInBandAllowed(bool b) {
-				allowInBand = b;
+				allowInBand_ = b;
 				return *this;
 			}
 
 			bool isInBandAllowed() const {
-				return allowInBand;
+				return allowInBand_;
 			}
+
+			FileTransferOptions& withAssistedAllowed(bool b) {
+				allowAssisted_ = b;
+				return *this;
+			}
+
+			bool isAssistedAllowed() const {
+				return allowAssisted_;
+			}
+
+			FileTransferOptions& withProxiedAllowed(bool b) {
+				allowProxied_ = b;
+				return *this;
+			}
+
+			bool isProxiedAllowed() const {
+				return allowProxied_;
+			}
+
+			FileTransferOptions& withDirectAllowed(bool b) {
+				allowDirect_ = b;
+				return *this;
+			}
+
+			bool isDirectAllowed() const {
+				return allowDirect_;
+			}
+
+
 
 			SWIFTEN_DEFAULT_COPY_ASSIGMNENT_OPERATOR(FileTransferOptions)
 
 		private:
-			bool allowInBand;
+			bool allowInBand_;
+			bool allowAssisted_;
+			bool allowProxied_;
+			bool allowDirect_;
 	};
 }
