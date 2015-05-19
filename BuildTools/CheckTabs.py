e86a46545e23ad824d7013c3c57bd83a52ade9af
Tobias Markmann <tm@ayena.de>
2015-05-19 10:45:22 +0200

    Enforce tab-only indentation in Python and SCons files
    This also converts CheckTabs.py script to tab indentation.
    
    Test-Information:
    
    Ran the script over all files in our repo and it detected space and
    mixed indented Python files.
    
    Change-Id: Ifed86d64c8e87ed55e85ef4d35157e312b937291

--- BuildTools/CheckTabs.py
+++ BuildTools/CheckTabs.py
@@ -5,27 +5,27 @@ import os, sys
 foundExpandedTabs = False
 
 filename = sys.argv[1]
-if (filename.endswith(".cpp") or filename.endswith(".h")) and not "3rdParty" in filename : 
-  file = open(filename, "r")
-  contents = []
-  contentsChanged = False
-  for line in file.readlines() :
-    newline = ""
-    previousChar = None
-    pastInitialSpace = False
-    for char in line :
-      if not pastInitialSpace :
-        if char == ' ' and previousChar == ' ' :
-          contentsChanged = True
-          previousChar = '\t'
-          continue
-        pastInitialSpace = (char != ' ')
-      if previousChar :
-        newline += previousChar
-      previousChar = char
-    if previousChar :
-      newline += previousChar
-    contents.append(newline)
-  file.close()
-  if contentsChanged :
-    sys.exit(-1)
+if (filename.endswith(".cpp") or filename.endswith(".h") or filename.endswith(".py") or filename.endswith("SConscript") or filename.endswith("SConscript.boot") or filename.endswith("SConstruct")) and not "3rdParty" in filename : 
+	file = open(filename, "r")
+	contents = []
+	contentsChanged = False
+	for line in file.readlines() :
+		newline = ""
+		previousChar = None
+		pastInitialSpace = False
+		for char in line :
+			if not pastInitialSpace :
+				if char == ' ' and previousChar == ' ' :
+					contentsChanged = True
+					previousChar = '\t'
+					continue
+				pastInitialSpace = (char != ' ')
+			if previousChar :
+				newline += previousChar
+			previousChar = char
+		if previousChar :
+			newline += previousChar
+		contents.append(newline)
+	file.close()
+	if contentsChanged :
+		sys.exit(-1)
