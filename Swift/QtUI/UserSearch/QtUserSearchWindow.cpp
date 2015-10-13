swift/swift

commit 9b48a737db5237ecdec062a64b65e6a90c1f9cde

Reset potential warning when opening 'Add Contact' dialog
Test-Information:

Tested by entering an existing JID, closing the dialog window and
reopening it.

Change-Id: I6c24ba319a062d19f2144c70fcc94fe77d36befe

Swift/QtUI/UserSearch/QtUserSearchWindow.cpp
@@ -304,6 +304,7 @@ void QtUserSearchWindow::handleOnSearchedJIDSelected(const Contact::ref& contact
 
 void QtUserSearchWindow::show() {
 	clear();
+	setWarning(boost::optional<std::string>());
 	QWidget::show();
 }
 

commit 39f291afd3cab9d6c1e7cdf7f0f5476ef6747abd

Fix crash in user search dialog for invitation use case
Commit 9b48a73 introduced a bug because it always assumed
the 'Add contact' user case.

Test-Information:

Retested the add contact case described in commit 9b48a73 and
tested MUC invitation by DND. Neither crashes anymore.

Change-Id: Ifab356e6ad9a52365ed15dd46e4e94492bdc60bf

Swift/QtUI/UserSearch/QtUserSearchWindow.cpp
@@ -304,7 +304,9 @@ void QtUserSearchWindow::handleOnSearchedJIDSelected(const Contact::ref& contact
 
 void QtUserSearchWindow::show() {
 	clear();
-	setWarning(boost::optional<std::string>());
+	if (type_ == AddContact) {
+		setWarning(boost::optional<std::string>());
+	}
 	QWidget::show();
 }
 