0c771572b62343dce8273966e76d97aef2018709
Michael Vetter <jubalh@openmailbox.org>
2015-12-26 23:57:42 +0100

    Add openSUSE to InstallSwiftDependencies script
    License:
    This patch is BSD-licensed, see Documentation/Licenses/BSD-simplified.txt for details.

--- BuildTools/InstallSwiftDependencies.sh
+++ BuildTools/InstallSwiftDependencies.sh
@@ -14,9 +14,12 @@ then
 	elif [ "$SYSTEM_DISTRO" == "Ubuntu" ]
 	then
 		sudo apt-get install pkg-config libssl-dev qt5-default libqt5x11extras5-dev libqt5webkit5-dev qtmultimedia5-dev qttools5-dev-tools
-	elif [ "$SYSTEM_DISTRO" == "Arch" ] 
+	elif [ "$SYSTEM_DISTRO" == "Arch" ]
 	then
 		sudo pacman -S qt5-base qt5-x11extras qt5-webkit qt5-multimedia qt5-tools
+	elif [ "$SYSTEM_DISTRO" == "openSUSE project" ]
+	then
+		sudo zypper in pkg-config libopenssl-devel libQt5Core-devel libQt5WebKit5-devel libQt5WebKitWidgets-devel libqt5-qtmultimedia-devel libqt5-qtx11extras-devel libqt5-qttools-devel libQt5Gui-devel libQt5Network-devel libQt5DBus-devel
 	else
 		echo "Unsupported Linux distribution."
 	fi
