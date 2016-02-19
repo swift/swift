51d6af8ca422dca26ffe01756ebb3a9f4ffd8b91
Tobias Markmann <tm@ayena.de>
2016-02-19 12:11:37 +0100

    Add workaround for building with Boost 1.60 as C++11
    Test-Information:
    
    Successfully built Swiften as C++11 with external Boost 1.60
    on OS X 10.11.3.
    
    Change-Id: I2c5a7c22f5f8c14ee7befcaeb84b7d1f093932f6

--- Swiften/EventLoop/Cocoa/CocoaEvent.h
+++ Swiften/EventLoop/Cocoa/CocoaEvent.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2013 Isode Limited.
+ * Copyright (c) 2010-2016 Isode Limited.
  * All rights reserved.
  * See the COPYING file for more information.
  */
@@ -7,6 +7,10 @@
 #pragma once
 
 #include <Foundation/Foundation.h>
+
+// The following line is a workaround for a bug in Boost 1.60 when building as C++11.
+// See ticket #11897 and #11863 in Boost's bug tracker.
+#undef check
 
 namespace Swift {
 	class CocoaEventLoop;
