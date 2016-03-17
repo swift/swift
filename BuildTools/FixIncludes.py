141b0cb28e2b830553cbb654499cce3a7a8a1907
Tobias Markmann <tm@ayena.de>
2016-03-17 17:54:34 +0100

    Put Sluift headers in their own group with FixIncludes.py
    Test-Information:
    
    Ran FixIncludes over some Sluift files and it sorted them
    as expected, meaning not grouped together with project
    external includes.
    
    Change-Id: I035381f4560b3c13f1a1feaefad7ecd8e268640b

--- BuildTools/FixIncludes.py
+++ BuildTools/FixIncludes.py
@@ -19,7 +19,7 @@ c_stdlib_headers = Set(["assert.h",  "limits.h",  "signal.h",  "stdlib.h", "ctyp
 cpp_stdlib_headers = Set(["algorithm", "fstream", "list", "regex", "typeindex", "array", "functional", "locale", "set", "typeinfo", "atomic", "future", "map", "sstream", "type_traits", "bitset", "initializer_list", "memory", "stack", "unordered_map", "chrono", "iomanip", "mutex", "stdexcept", "unordered_set", "codecvt", "ios", "new", "streambuf", "utility", "complex", "iosfwd", "numeric", "string", "valarray", "condition_variable", "iostream", "ostream", "strstream", "vector", "deque", "istream", "queue", "system_error", "exception", "iterator", "random", "thread", "forward_list", "limits", "ratio", "tuple", "cassert", "ciso646", "csetjmp", "cstdio", "ctime", "cctype", "climits", "csignal", "cstdlib", "cwchar", "cerrno", "clocale", "cstdarg", "cstring", "cwctype", "cfloat", "cmath", "cstddef"])
 
 class HeaderType:
-		PRAGMA_ONCE, CORRESPONDING_HEADER, C_STDLIB, CPP_STDLIB, BOOST, QT, OTHER, SWIFTEN, SWIFT_CONTROLLERS, SWIFTOOLS, SWIFT = range(11)
+		PRAGMA_ONCE, CORRESPONDING_HEADER, C_STDLIB, CPP_STDLIB, BOOST, QT, OTHER, SWIFTEN, SWIFT_CONTROLLERS, SLUIFT, SWIFTOOLS, SWIFT = range(12)
 
 def findHeaderBlock(lines):
 	start = False
@@ -66,6 +66,9 @@ def fileNameToHeaderType(name):
 
 	if name.startswith("Swift/Controllers"):
 		return HeaderType.SWIFT_CONTROLLERS
+
+	if name.startswith("Sluift"):
+		return HeaderType.SLUIFT
 
 	if name.startswith("SwifTools"):
 		return HeaderType.SWIFTOOLS
