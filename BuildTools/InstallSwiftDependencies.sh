a3747d20b3753e643c76f4051b6789ce9ffcd11a
Tobias Markmann <tm@ayena.de>
2016-08-15 09:52:17 +0200

    Update InstallSwiftDependencies.sh
    Sync dependencies for Debian and Ubuntu.
    Add development packages for QtSvg for all systems.
    
    Test-Information:
    
    None.
    
    Change-Id: Ied0f160942bbf43304cec119c8c0c4bf84c39050

--- BuildTools/InstallSwiftDependencies.sh
+++ BuildTools/InstallSwiftDependencies.sh
@@ -10,20 +10,20 @@ then
     SYSTEM_DISTRO=$(lsb_release -i -s)
     if [ "$SYSTEM_DISTRO" == "Debian" ]
     then
-        sudo apt-get install pkg-config libssl-dev qt5-default libqt5x11extras5-dev libqt5webkit5-dev qtmultimedia5-dev qttools5-dev-tools qt5-image-formats-plugins libqt5svg5 libminiupnpc-dev libnatpmp-dev libhunspell-dev
+        sudo apt-get install pkg-config libssl-dev qt5-default libqt5x11extras5-dev libqt5webkit5-dev qtmultimedia5-dev qttools5-dev-tools qt5-image-formats-plugins libqt5svg5-dev libminiupnpc-dev libnatpmp-dev libhunspell-dev
     elif [ "$SYSTEM_DISTRO" == "Ubuntu" ]
     then
-        sudo apt-get install pkg-config libssl-dev qt5-default libqt5x11extras5-dev libqt5webkit5-dev qtmultimedia5-dev qttools5-dev-tools qt5-image-formats-plugins libqt5svg5 libhunspell-dev
+        sudo apt-get install pkg-config libssl-dev qt5-default libqt5x11extras5-dev libqt5webkit5-dev qtmultimedia5-dev qttools5-dev-tools qt5-image-formats-plugins libqt5svg5-dev libminiupnpc-dev libnatpmp-dev libhunspell-dev
     elif [ "$SYSTEM_DISTRO" == "Arch" ]
     then
-        sudo pacman -S qt5-base qt5-x11extras qt5-webkit qt5-multimedia qt5-tools hunspell
+        sudo pacman -S qt5-base qt5-x11extras qt5-webkit qt5-multimedia qt5-tools qt5-svg hunspell
     elif [ "$SYSTEM_DISTRO" == "openSUSE project" ]
     then
-        sudo zypper in pkg-config libopenssl-devel libQt5Core-devel libQt5WebKit5-devel libQt5WebKitWidgets-devel libqt5-qtmultimedia-devel libqt5-qtx11extras-devel libqt5-qttools-devel libQt5Gui-devel libQt5Network-devel libQt5DBus-devel python-xml hunspell-devel libQt5Svg5
+        sudo zypper in pkg-config libopenssl-devel libQt5Core-devel libQt5WebKit5-devel libQt5WebKitWidgets-devel libqt5-qtmultimedia-devel libqt5-qtx11extras-devel libqt5-qttools-devel libQt5Gui-devel libQt5Network-devel libQt5DBus-devel libQt5Svg5 python-xml hunspell-devel
     elif [ "$SYSTEM_DISTRO" == "Fedora" ]
     then
         sudo dnf groups install "C Development Tools and Libraries"
-        sudo dnf install openssl-devel qt5-qtbase-devel qt5-linguist qt5-qtwebkit-devel qt5-qtmultimedia-devel qt5-qtx11extras-devel hunspell-devel
+        sudo dnf install openssl-devel qt5-qtbase-devel qt5-linguist qt5-qtwebkit-devel qt5-qtmultimedia-devel qt5-qtx11extras-devel qt5-qtsvg-devel hunspell-devel
     else
         echo "Unsupported Linux distribution."
     fi
