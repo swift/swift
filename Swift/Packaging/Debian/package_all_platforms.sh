5035a59073e07a2dbe90dd94008e9d8b5a2cbd0a
Tobias Markmann <tm@ayena.de>
2017-02-07 16:11:38 +0100

    Remove trusty and add yakkety to package_all_platforms.sh
    Test-Information:
    
    None.
    
    Change-Id: Iaa0bad47c104b9407329d69329f3409af0d62e6d

--- Swift/Packaging/Debian/package_all_platforms.sh
+++ Swift/Packaging/Debian/package_all_platforms.sh
@@ -4,8 +4,8 @@
 set -e
 
 ## To get to this stage you need to already have the pbuilder-dist stuff installed, and have already run:
-# for distro in squeeze sid lucid maverick; do
-#  for arch in i386 amd64; do
+# for distro in xenial yakkety jessie sid; do
+#  for arch in amd64; do
 #   pbuilder-dist $distro $arch create
 #  done
 # done
@@ -24,7 +24,7 @@ export SWIFT_FORCE_LUCID="yep"
 unset SWIFT_FORCE_LUCID
 ./package.sh
 
-for distro in trusty xenial jessie sid; do
+for distro in xenial yakkety jessie sid; do
 	for arch in amd64; do
 		pbuilder-dist $distro $arch build *.dsc
 	done
