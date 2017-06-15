e142d11c3fa6ae2bdfc95ee9cea54bb0000917c9
Tarun Gupta <tarun1995gupta@gmail.com>
2017-06-15 16:34:26 +0530

    Adds MIX Join Element, its Parser and Serializer.
    Add MIXDestroySerializer to FullPayloadSerializerCollection.
    
    License:
    This patch is BSD-licensed, see Documentation/Licenses/BSD-simplified.txt for details.
    
    Test-Information:
    Added tests for MIX Join Parser and Serializer based on examples in XEP 0369, which passes.
    Tested on Ubuntu 16.04 LTS.
    
    Change-Id: Ibf7446dd65714dccdd39c485549fd9fb9faba40c

---
+++ Swiften/Serializer/PayloadSerializers/MIXJoinSerializer.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2017 Tarun Gupta
+ * Licensed under the simplified BSD license.
+ * See Documentation/Licenses/BSD-simplified.txt for more information.
+ */
+
+#include <Swiften/Serializer/PayloadSerializers/MIXJoinSerializer.h>
+
+#include <memory>
+
+#include <Swiften/Serializer/PayloadSerializers/MIXSubscribeSerializer.h>
+#include <Swiften/Serializer/PayloadSerializers/FormSerializer.h>
+#include <Swiften/Serializer/XML/XMLElement.h>
+#include <Swiften/Serializer/XML/XMLRawTextNode.h>
+
+using namespace Swift;
+
+MIXJoinSerializer::MIXJoinSerializer() {
+}
+
+MIXJoinSerializer::~MIXJoinSerializer() {
+}
+
+std::string MIXJoinSerializer::serializePayload(std::shared_ptr<MIXJoin> payload) const {
+    if (!payload) {
+        return "";
+    }
+    XMLElement element("join", "urn:xmpp:mix:1");
+    if (payload->getChannel()) {
+        element.setAttribute("channel", *payload->getChannel());
+    }
+    if (payload->getJID()) {
+        element.setAttribute("jid", *payload->getJID());
+    }
+    for (const auto& item : payload->getSubscriptions()) {
+        element.addNode(std::make_shared<XMLRawTextNode>(MIXSubscribeSerializer().serialize(item)));
+    }
+    if (payload->getForm()) {
+        element.addNode(std::make_shared<XMLRawTextNode>(FormSerializer().serialize(payload->getForm())));
+    }
+    return element.serialize();
+}
