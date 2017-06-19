a70abdc71877142cb8f081d82d528cdf2fdc43be
Tarun Gupta <tarun1995gupta@gmail.com>
2017-06-19 15:12:31 +0530

    Adds MIXUserPreference Element, its Parser and Serializer
    License:
    This patch is BSD-licensed, see Documentation/Licenses/BSD-simplified.txt for details.
    
    Test-Information:
    Added tests for MIXUserPreference Parser and Serializer based on examples in XEP 0369, which passes.
    
    Change-Id: I06595325f4cc2b34d5ab5a93a6caa00330fe0737

--- Swiften/Parser/PayloadParsers/FullPayloadParserFactoryCollection.cpp
+++ Swiften/Parser/PayloadParsers/FullPayloadParserFactoryCollection.cpp
@@ -55,6 +55,7 @@
 #include <Swiften/Parser/PayloadParsers/MIXCreateParser.h>
 #include <Swiften/Parser/PayloadParsers/MIXDestroyParser.h>
 #include <Swiften/Parser/PayloadParsers/MIXJoinParserFactory.h>
+#include <Swiften/Parser/PayloadParsers/MIXUserPreferenceParser.h>
 #include <Swiften/Parser/PayloadParsers/MUCAdminPayloadParser.h>
 #include <Swiften/Parser/PayloadParsers/MUCDestroyPayloadParser.h>
 #include <Swiften/Parser/PayloadParsers/MUCInvitationPayloadParser.h>
@@ -136,6 +137,7 @@ FullPayloadParserFactoryCollection::FullPayloadParserFactoryCollection() {
     factories_.push_back(std::make_shared<MIXParticipantParserFactory>());
     factories_.push_back(std::make_shared<GenericPayloadParserFactory<MIXDestroyParser> >("destroy", "urn:xmpp:mix:1"));
     factories_.push_back(std::make_shared<GenericPayloadParserFactory<MIXCreateParser> >("create", "urn:xmpp:mix:1"));
+    factories_.push_back(std::make_shared<GenericPayloadParserFactory<MIXUserPreferenceParser> >("user-preference", "urn:xmpp:mix:1"));
     factories_.push_back(std::make_shared<MUCUserPayloadParserFactory>(this));
     factories_.push_back(std::make_shared<MUCOwnerPayloadParserFactory>(this));
     factories_.push_back(std::make_shared<GenericPayloadParserFactory<MUCInvitationPayloadParser> >("x", "jabber:x:conference"));
