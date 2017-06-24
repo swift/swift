7f904a54248724625286dc3c4e44cd5598e17189
Tarun Gupta <tarun1995gupta@gmail.com>
2017-06-24 22:37:44 +0530

    Adds MIX Destroy Element, its Parser and Serializer.
    License:
    This patch is BSD-licensed, see Documentation/Licenses/BSD-simplified.txt for details.
    
    Test-Information:
    Added tests for MIX Destroy Parser and Serializer based on examples in XEP 0369, which passes.
    
    Change-Id: I56810eb2fc26cc6aeb5e1cfb2a32e312f0607ba9

---
+++ Swiften/Parser/PayloadParsers/MIXDestroyParser.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017 Tarun Gupta
+ * Licensed under the simplified BSD license.
+ * See Documentation/Licenses/BSD-simplified.txt for more information.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include <Swiften/Base/API.h>
+#include <Swiften/Base/Override.h>
+#include <Swiften/Elements/MIXDestroy.h>
+#include <Swiften/Parser/GenericPayloadParser.h>
+
+namespace Swift {
+    class PayloadParser;
+
+    class SWIFTEN_API MIXDestroyParser : public GenericPayloadParser<MIXDestroy> {
+        public:
+            MIXDestroyParser();
+            virtual ~MIXDestroyParser();
+
+            virtual void handleStartElement(const std::string& element, const std::string&, const AttributeMap& attributes) SWIFTEN_OVERRIDE;
+            virtual void handleEndElement(const std::string& element, const std::string&) SWIFTEN_OVERRIDE;
+            virtual void handleCharacterData(const std::string& data) SWIFTEN_OVERRIDE;
+
+        private:
+            int level_;
+    };
+}
