67c4e95c737e48504d622b3eb02394610231b05d
Tarun Gupta <tarun1995gupta@gmail.com>
2017-06-26 18:48:09 +0530

    Add MIXRegisterNick Element, its Parser and Serializer
    License:
    This patch is BSD-licensed, see Documentation/Licenses/BSD-simplified.txt for details.
    
    Test-Information:
    Added tests for MIXRegisterNick Parser and Serializer based on examples in XEP 0369, which passes.
    Tested on Ubuntu 16.04 LTS.
    
    Change-Id: I80796db9d540c58296afa3454f29e1fb81d04615

--- Swiften/Serializer/PayloadSerializers/FullPayloadSerializerCollection.cpp
+++ Swiften/Serializer/PayloadSerializers/FullPayloadSerializerCollection.cpp
@@ -48,6 +48,7 @@
 #include <Swiften/Serializer/PayloadSerializers/MIXJoinSerializer.h>
 #include <Swiften/Serializer/PayloadSerializers/MIXDestroySerializer.h>
 #include <Swiften/Serializer/PayloadSerializers/MIXParticipantSerializer.h>
+#include <Swiften/Serializer/PayloadSerializers/MIXRegisterNickSerializer.h>
 #include <Swiften/Serializer/PayloadSerializers/MIXCreateSerializer.h>
 #include <Swiften/Serializer/PayloadSerializers/MIXPayloadSerializer.h>
 #include <Swiften/Serializer/PayloadSerializers/MIXSetNickSerializer.h>
@@ -164,6 +165,7 @@ FullPayloadSerializerCollection::FullPayloadSerializerCollection() {
     serializers_.push_back(new ForwardedSerializer(this));
     serializers_.push_back(new MIXParticipantSerializer());
     serializers_.push_back(new MIXCreateSerializer());
+    serializers_.push_back(new MIXRegisterNickSerializer());
     serializers_.push_back(new MIXPayloadSerializer());
     serializers_.push_back(new MIXSetNickSerializer());
     serializers_.push_back(new MIXUserPreferenceSerializer());
