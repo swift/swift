99daa71715e714ced167c2ef474a03d02d580cf7
Tarun Gupta <tarun1995gupta@gmail.com>
2017-07-08 02:39:17 +0530

    Add Channel Join and Leave capability to MIX
    Add UpdateSubscription feature for subscribing to additional MIX nodes
    Add Joining a channel with a preference form
    Add requesting user preference form and updating the preferences
    
    License:
    This patch is BSD-licensed, see Documentation/Licenses/BSD-simplified.txt for details.
    
    Test-Information:
    Tests added for joining and leaving a channel as in XEP-0369, which passes.
    Tests also added for updating subscription, joining channel with preference form and requesting user preference form as in XEP-0369, which passes.
    Tested on Ubuntu 16.04 LTS.
    
    Change-Id: Ibc2737f6154eeee1a85e98cb5f80c8bdbad35dcd

---
+++ Swiften/MIX/MIXImpl.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2017 Tarun Gupta
+ * Licensed under the simplified BSD license.
+ * See Documentation/Licenses/BSD-simplified.txt for more information.
+ */
+
+#pragma once
+
+#include <Swiften/MIX/MIX.h>
+
+namespace Swift {
+    class StanzaChannel;
+    class IQRouter;
+
+    class SWIFTEN_API MIXImpl : public MIX {
+        public:
+            using ref = std::shared_ptr<MIXImpl>;
+
+        public:
+            MIXImpl(const JID& ownJID, const JID& channelJID, IQRouter* iqRouter);
+            virtual ~MIXImpl();
+
+            /**
+             * Returns the (bare) JID of the user.
+             */
+            virtual JID getJID() const {
+                return ownJID_.toBare();
+            }
+
+            /**
+             * Returns the JID of MIX channel.
+             */
+            virtual JID getChannelJID() const {
+                return channelJID_;
+            }
+
+            virtual void joinChannel(const std::unordered_set<std::string>& nodes) override;
+
+            virtual void joinChannelWithPreferences(const std::unordered_set<std::string>& nodes, Form::ref form) override;
+
+            virtual void updateSubscription(const std::unordered_set<std::string>& nodes) override;
+
+            virtual void leaveChannel() override;
+
+            virtual void requestPreferencesForm() override;
+
+            virtual void updatePreferences(Form::ref form) override;
+
+        private:
+            void handleJoinResponse(MIXJoin::ref, ErrorPayload::ref);
+            void handleLeaveResponse(MIXLeave::ref, ErrorPayload::ref);
+            void handleUpdateSubscriptionResponse(MIXUpdateSubscription::ref, ErrorPayload::ref);
+            void handlePreferencesFormReceived(MIXUserPreference::ref, ErrorPayload::ref);
+            void handlePreferencesResultReceived(MIXUserPreference::ref /*payload*/, ErrorPayload::ref error);
+
+        private:
+            JID ownJID_;
+            JID channelJID_;
+            IQRouter* iqRouter_;
+    };
+}
