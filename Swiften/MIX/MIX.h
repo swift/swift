44fe43050b7d69c4880e2dbfdd481a5741c79ef0
Tarun Gupta <tarun1995gupta@gmail.com>
2017-07-08 02:39:17 +0530

    Adds Channel Join and Leave capability to MIX
    Updates MIXJoin Element list of subscriptions to an unordered_set.
    Remove redundant MIXSubscribe Element.
    
    License:
    This patch is BSD-licensed, see Documentation/Licenses/BSD-simplified.txt for details.
    
    Test-Information:
    Tests added for joining and leaving a channel as in XEP-0369, which passes.
    Tested on Ubuntu 16.04 LTS.
    
    Change-Id: Ibc2737f6154eeee1a85e98cb5f80c8bdbad35dcd

---
+++ Swiften/MIX/MIX.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2017 Tarun Gupta
+ * Licensed under the simplified BSD license.
+ * See Documentation/Licenses/BSD-simplified.txt for more information.
+ */
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_set>
+
+#include <boost/signals2.hpp>
+
+#include <Swiften/Base/API.h>
+#include <Swiften/JID/JID.h>
+#include <Swiften/Elements/MIXJoin.h>
+#include <Swiften/Elements/MIXLeave.h>
+#include <Swiften/Elements/ErrorPayload.h>
+
+namespace Swift {
+    class SWIFTEN_API MIX {
+        public:
+            using ref = std::shared_ptr<MIX>;
+
+        public:
+            virtual ~MIX();
+
+            /**
+            * Join a MIX channel and subscribe to nodes.
+            */
+            virtual void joinChannelAndSubscribe(const JID &channel, const std::unordered_set<std::string>& nodes) = 0;
+            virtual void joinChannel(const JID &channel) = 0;
+
+            virtual void leaveChannel(const JID &channel) = 0;
+
+        public:
+            boost::signals2::signal<void (MIXJoin::ref /* joinResponse */)> onJoinComplete;
+            boost::signals2::signal<void (ErrorPayload::ref /* joinError */)> onJoinFailed;
+            boost::signals2::signal<void (MIXLeave::ref /* leaveResponse */)> onLeaveComplete;
+            boost::signals2::signal<void (ErrorPayload::ref /* leaveError */)> onLeaveFailed;
+    };
+}
