swift/swift

commit 2b185f0b25aede6e2f40a678647c78bb8e7f8638

Allow define the debian repositories to upload
This patch uses SWIFT_PACKAGE_PLATFORMS variable and only updates the
repositories that are defined there.

Test-Information
Tested the script on Ubuntu 16.04 when the variable is/not defined

Change-Id: Ie8f65441f558e3ac663eb8493cedd8c0d2630753

Swift/Packaging/Debian/update_debian_repo.sh
@@ -73,11 +73,22 @@ command -v reprepro >/dev/null 2>&1 || { echo >&2 "This script requires aptly bu
 
 mkdir -p $APT_REPO_ROOT
 
+if [ -z ${SWIFT_PACKAGE_PLATFORMS+x} ]; then
+	platformsarray=( xenial yakkety jessie sid )
+else
+	platformsarray=( $SWIFT_PACKAGE_PLATFORMS )
+fi
+
 # distros
 for full_distribution_path in $INCOMING_FOLDER/{debian,ubuntu}/*; do
 	distro_version=`basename $full_distribution_path`
 	distro_name=$(basename `dirname $full_distribution_path`)
 	distro_reprepro_root=${APT_REPO_ROOT}/${distro_name}/${distro_version}
+
+	if ! [[ $SWIFT_PACKAGE_PLATFORMS == *"$distro_name"* ]]; then
+		echo "$distro_name was not found in SWIFT_PACKAGE_PLATFORMS. Skipping..."
+		continue
+	fi
 
 	# ensure reprepro diretctory for this distribution version is present
 	if [ ! -d "$distro_preprepro_root" ]; then
@@ -89,7 +100,7 @@ for full_distribution_path in $INCOMING_FOLDER/{debian,ubuntu}/*; do
 		write_reprepo_conf_incoming_to_file "${distro_reprepro_root}/conf/incoming" "$full_distribution_path"
 	fi
 
-	# This is workaround for https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=808558 
+	# This is workaround for https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=808558
 	# and https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=843402
 	if [ "$distro_name/$distro_version" = "debian/sid" ]; then
 		sed -i '/dbgsym/ d' $full_distribution_path/*.changes

commit 842a9651b9ead023bc079decfe3bce9f9257bee7

Fix the debian repository update script
This will use the distro_version instead of the distro_name variable
in the script. This fixes an error that was introduced in commit
2b185f0b25aede6e2f40a678647c78bb8e7f8638

Test-Information:
None

Change-Id: I943a680c49f70310ff525ee60f31b23641ccb2f5

Swift/Packaging/Debian/update_debian_repo.sh
@@ -85,8 +85,8 @@ for full_distribution_path in $INCOMING_FOLDER/{debian,ubuntu}/*; do
 	distro_name=$(basename `dirname $full_distribution_path`)
 	distro_reprepro_root=${APT_REPO_ROOT}/${distro_name}/${distro_version}
 
-	if ! [[ $SWIFT_PACKAGE_PLATFORMS == *"$distro_name"* ]]; then
-		echo "$distro_name was not found in SWIFT_PACKAGE_PLATFORMS. Skipping..."
+	if ! [[ $SWIFT_PACKAGE_PLATFORMS == *"$distro_version"* ]]; then
+		echo "$distro_version was not found in SWIFT_PACKAGE_PLATFORMS. Skipping..."
 		continue
 	fi
 